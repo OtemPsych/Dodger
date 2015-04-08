20c3984a2db2d6560aed65bf4d16177f5a6f9c58
Filippos Gleglakos <gleglakos.filippos@gmail.com>
2015-04-08 15:25:57 +0300

    Added exception-catching

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include "Game.h"
 
+#include <iostream>
+
 int main()
 {
     srand(time(0));
 
-    Game game;
-    game.run();
+    try {
+        Game game;
+        game.run();
+    }
+    catch (std::exception& e) {
+        std::cout << "EXCEPTION: " << e.what() << std::endl;
+    }
 
     return 0;
 }
